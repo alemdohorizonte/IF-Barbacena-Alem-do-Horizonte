d1dfdb60c0f70b1ab41a7b9f3b69a32b61a1eb58
João Pedro <jpedroo150@gmail.com>
2020-07-23 00:03:13 -0300

    [up] dynamic server host

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: window.location.protocol + '//' + window.location.hostname + ':3333',
   timeout: 30000,
 });
 
